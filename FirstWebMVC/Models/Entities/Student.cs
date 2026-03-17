347ed24122fd616bb478e5e45318118943e47775
HongAnh <sudungchucaituadenz@gmail.com>
2026-03-17 18:13:37 +0700

    Buổi 7

--- FirstWebMVC/Models/Entities/Student.cs
+++ FirstWebMVC/Models/Entities/Student.cs
@@ -5,7 +5,14 @@ namespace FirstWebMVC.Models.Entities
     public class Student
     {
         [Key]
-        public int? StudentCode { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập mã sinh viên!")]
+        [RegularExpression(@"^[0-9]{10}$", ErrorMessage = "Sai định dạng mã sinh viên")]
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "Mã sinh viên không hợp lệ")]
+        public string? StudentCode { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập tên đầy đủ!")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Tên phải nhỏ hơn 50 ký tự và lớn hơn 3 ký tự")]
         public string? FullName { get; set; }
     }
 }
